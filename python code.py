954025930366ea99b590021ef0d777addd94cdf9
vinayakhegde1 <73926849+vinayakhegde1@users.noreply.github.com>
2020-11-14 23:41:06 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,123 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> demo_string = "Itvoyagers is a blog"
+>>> print('demo_string[0:5]',demo_string[0:5])
+demo_string[0:5] Itvoy
+>>> demo_string = "example of string slicing"
+>>> print('demo_string[0:5]',demo_string[0:5])
+demo_string[0:5] examp
+>>> print('demo_string[9:12]',demo_string[9:12])
+demo_string[9:12] f s
+>>> print('demo_string[5:12]',demo_string[5;12])
+SyntaxError: invalid syntax
+>>> print('demo_string[5:12]',demo_string[5:12])
+demo_string[5:12] le of s
+>>> print('demo_string[:3]',demo_string[:3])
+demo_string[:3] exa
+>>> print('demo_string[3:]',demo_string[3:])
+print('demo_string[3:3]',demo_string[3:3])
+print('demo_string[:]',demo_string[:])
+print('demo_string[0]',demo_string[0])
+print('demo_string[10]',demo_string[10])
+print('demo_string[5]',demo_string[5])
+print('demo_string[5:14:2]',demo_string[5:14:2])
+print('demo_string[5::2]',demo_string[5::2])
+print('demo_string[:14:2]',demo_string[:14:2])
+print('demo_string[5:1:-2]',demo_string[5:1:-2])
+print('demo_string[5:1:-1]',demo_string[5:1:-1])
+print('demo_string[::]',demo_string[::])
+SyntaxError: multiple statements found while compiling a single statement
+>>> print('demo_string[3:]',demo_string[3:])
+demo_string[3:] mple of string slicing
+>>> print('demo_string[3:3]',demo_string[3:3])
+demo_string[3:3] 
+>>> print('demo_string[:]',demo_string[:])
+demo_string[:] example of string slicing
+>>> print('demo_string[0]',demo_string[0])
+demo_string[0] e
+>>> print('demo_string[5:14:2]',demo_string[5:14:2])
+demo_string[5:14:2] l fsr
+>>> print('demo_string[5::2]',demo_string[5::2])
+demo_string[5::2] l fsrn lcn
+>>> print('demo_string[:14:2]',demo_string[:14:2])
+demo_string[:14:2] eapeo t
+>>> print('demo_string[5:1:-2]',demo_string[5:1:-2])
+demo_string[5:1:-2] lm
+>>> print('demo_string[5:1:-1]',demo_string[5:1:-1])
+demo_string[5:1:-1] lpma
+>>> print('demo_string[::]',demo_string[::])
+demo_string[::] example of string slicing
+>>> #string slicing
+demo_string = "Itvoyagers is a blog"
+print('demo_string[0:5]',demo_string[0:5])
+print('demo_string[9:12]',demo_string[9:12])
+print('demo_string[5:12]',demo_string[5:12])
+print('demo_string[:3]',demo_string[:3])
+print('demo_string[3:]',demo_string[3:])
+print('demo_string[3:3]',demo_string[3:3])
+print('demo_string[:]',demo_string[:])
+print('demo_string[0]',demo_string[0])
+print('demo_string[10]',demo_string[10])
+print('demo_string[5]',demo_string[5])
+print('demo_string[5:14:2]',demo_string[5:14:2])
+print('demo_string[5::2]',demo_string[5::2])
+print('demo_string[:14:2]',demo_string[:14:2])
+print('demo_string[5:1:-2]',demo_string[5:1:-2])
+print('demo_string[5:1:-1]',demo_string[5:1:-1])
+print('demo_string[::]',demo_string[::])
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> 
+>>> demo_string = "python programming"
+>>> len("python programming")
+18
+>>> 
+>>> print('demo_string[0:5]',demo_string[0:5])
+print('demo_string[9:12]',demo_string[9:12])
+print('demo_string[5:12]',demo_string[5:12])
+print('demo_string[:3]',demo_string[:3])
+print('demo_string[3:]',demo_string[3:])
+print('demo_string[3:3]',demo_string[3:3])
+print('demo_string[:]',demo_string[:])
+print('demo_string[0]',demo_string[0])
+print('demo_string[10]',demo_string[10])
+print('demo_string[5]',demo_string[5])
+print('demo_string[5:14:2]',demo_string[5:14:2])
+print('demo_string[5::2]',demo_string[5::2])
+print('demo_string[:14:2]',demo_string[:14:2])
+print('demo_string[5:1:-2]',demo_string[5:1:-2])
+print('demo_string[5:1:-1]',demo_string[5:1:-1])
+print('demo_string[::]',demo_string[::])
+SyntaxError: multiple statements found while compiling a single statement
+>>> print('demo_string[9:12]',demo_string[9:12])
+demo_string[9:12] ogr
+>>> print('demo_string[5:12]',demo_string[5:12])
+demo_string[5:12] n progr
+>>> print('demo_string[:3]',demo_string[:3])
+demo_string[:3] pyt
+>>> print('demo_string[3:]',demo_string[3:])
+demo_string[3:] hon programming
+>>> print('demo_string[3:3]',demo_string[3:3])
+demo_string[3:3] 
+>>> print('demo_string[:]',demo_string[:])
+demo_string[:] python programming
+>>> print('demo_string[0]',demo_string[0])
+demo_string[0] p
+>>> v
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    v
+NameError: name 'v' is not defined
+>>> print('demo_string[5:14:2]',demo_string[5:14:2])
+demo_string[5:14:2] nporm
+>>> print('demo_string[5::2]',demo_string[5::2])
+demo_string[5::2] npormig
+>>> print('demo_string[:14:2]',demo_string[:14:2])
+demo_string[:14:2] pto rga
+>>> print('demo_string[5:1:-2]',demo_string[5:1:-2])
+demo_string[5:1:-2] nh
+>>> print('demo_string[5:1:-1]',demo_string[5:1:-1])
+demo_string[5:1:-1] noht
+>>> print('demo_string[::]',demo_string[::])
+demo_string[::] python programming
+>>> 
